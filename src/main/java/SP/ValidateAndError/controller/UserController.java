7a095e32b280ea03651f523cab93922d17ab9a06
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-12-28 11:56:31 +0530

    Controllers

--- src/main/java/SP/ValidateAndError/controller/UserController.java
+++ src/main/java/SP/ValidateAndError/controller/UserController.java
@@ -1,4 +1,35 @@
 package SP.ValidateAndError.controller;
 
+import SP.ValidateAndError.dto.UserRequest;
+import SP.ValidateAndError.entity.User;
+import SP.ValidateAndError.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> saveUser(@RequestBody UserRequest userRequest){
+        return new ResponseEntity<>(service.saveUser(userRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchAll")
+    public ResponseEntity<List<User>> getUser() {
+        return ResponseEntity.ok(service.getAllUser());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable int id) {
+        return ResponseEntity.ok(service.getUser(id));
+    }
 }
